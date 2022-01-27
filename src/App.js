213aa4a96fe3eb4bcbcae79d4b408cc517c77105
Rhyannon <rhyannonjoy@gmail.com>
2022-01-26 22:21:29 -0700

    added description to landing page, paired with logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from "./components/logo.png";
 import "./App.css";
+import description from "./components/description.png";
+import logo from "./components/logo.png";
 import SignIn from "./components/SignIn.js";
 import SignUp from "./components/SignUp.js";
 
@@ -12,6 +13,11 @@ function App() {
       </nav>
       <section className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={description}
+          className="App-description"
+          alt="description"
+        ></img>
       </section>
       <footer className="footer-section">
         ©Ada Developers Academy RJR 2022
