0085e3d23b9076ee4095150a44b5040c4d23cf74
Rhyannon <rhyannonjoy@gmail.com>
2022-02-01 22:09:12 -0700

    added ErrorPage components + styling

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,24 @@
+import "./ErrorPage.css";
+import ContactButton from "../components/buttons/ContactButton";
+import HomeButton from "../components/buttons/HomeButton";
+import page_not_found from "../images/page_not_found.png";
+import SignInButton from "../components/buttons/SignInButton";
+
 const ErrorPage = () => {
   return (
     <div>
-      <h1>This is the Error Page.</h1>
-      <h1>Page Not Found 404.</h1>
+      <div className="error-page-button-container">
+        <ContactButton />
+        <HomeButton />
+        <SignInButton />
+      </div>
+      <div>
+        <img
+          src={page_not_found}
+          className="page-not-found-image"
+          alt="404 Error Page Not Found"
+        ></img>
+      </div>
     </div>
   );
 };
