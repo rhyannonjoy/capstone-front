603a13e30550ede03c93eff27fbae59bc12da51e
Rhyannon <rhyannonjoy@gmail.com>
2022-02-01 23:32:32 -0700

    added minor styling to DashboardPage, needs work

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,18 +1,24 @@
 import "./DashboardPage.css";
 import ContactButton from "../components/buttons/ContactButton";
 import HomeButton from "../components/buttons/HomeButton";
+import pencil from "../images/pencil.png";
+import scribble from "../images/scribble.png";
 import SubmitQuestionForm from "../components/forms/SubmitQuestionForm";
 import { useParams } from "react-router-dom";
 
 const DashboardPage = () => {
   let { username } = useParams();
   return (
-    <div>
+    <div className="dashboard-body">
       <div className="button-container">
+        <img src={scribble} className="scribble-image" alt="scribble"></img>
+        <h1 className="dashboard-header">
+          Welcome to your Dashboard, {username}
+        </h1>
+        <img src={pencil} className="pencil-image" alt="pencil"></img>
         <ContactButton />
         <HomeButton />
       </div>
-      <h1 className="dashboard-header">This is {username}'s Dashboard</h1>
       <div className="submit-form">
         <SubmitQuestionForm />
       </div>
